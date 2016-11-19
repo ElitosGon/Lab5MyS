8acfbab28f668057904e96cdc5c26a340d25cef0
Ian <ian.orellana@usach.cl>
2016-11-19 07:34:27 -0300

    Se añaden comentarios

--- Lab5/Parte2/MH.cpp
+++ Lab5/Parte2/MH.cpp
@@ -7,29 +7,44 @@
 #define _USE_MATH_DEFINES
 using namespace std;
 
+// Función de prueba
 double function(double x){
 	double result = exp(-(x*x))*(2+sin(x*5)+sin(x*2));
 	return result;
 }
 
+
+// Función pedida en el punto 2.2.2
 double funcion2(double x){
 	double result = exp(-pow(x,3))*(8+cos(3*x)+sin(x)-2*M_PI);
 }
 
-double MHstep(double xo, double sig){
+
+// Función generadora de siguiente valor (punto actual, desviación)
+double MHstep(double xo, double ds){
+	// semilla random
 	std::default_random_engine generator;
+	// generador random de distribución uniforme (cotas)
   	std::uniform_real_distribution<double> distribution(0.0,1.0);
 
+  	// valor siguiente
   	double x1;
-	double xp=Normal(xo,sig);
+  	// se genera un número aleatorio en distribución normal (media,desviación)
+	double xp=Normal(xo,ds);
+	// probabilidad de aceptación (alpha)
 	double accprob= funcion2(xp)/funcion2(xo);
+	// alpha = min(1,probabilidad de aceptación)
 	if(accprob>1){
 		accprob=1;
 	}
+	// genera número en distribución uniforme (entre 0 y 1)
 	double u = distribution(generator);
+	// Se acepta si número generado es menor o igual a probabilidad de aceptación
 	if(u <= accprob){
-		x1 = xp;
+		// si se acepta el nuevo punto es candidato
+		x1 = xp; 
 	}else{
+		// si no se acepta el candidato es el mismo punto ingresado
 		x1 = xo;
 	}
 	return x1;
@@ -37,20 +52,31 @@ double MHstep(double xo, double sig){
 
 int main(){
  
-  int burnin = 0;
-  int lag = 1;
-  int samples = 1000;
-  int sd = 50;
-  double x = -1;
+ ///Parámetros
+  int burnin = 0; // burn-in iterations -> iteraciones previas para evitar mal punto de partida
+  int lag = 1; // lag entre muestreo -> para distribuciones con baja tasa de aceptación
+  int samples = 1000; // numero de muestras
+  int sd = 50; // desviación estándar
+  double x = -1; // punto de partida
 
-  for(int i=1;i<=burnin;i=i+1){
+  
+  // iteraciones previas (burn in)
+  for(int i=1;i<=burnin;i=i+1){ 
   		x = MHstep(x,sd);
   }
+
+
+  // ciclo de muestreo
   for(int i=1;i<=samples;i=i+1){
+
+  		// ciclo de lag entre muestreo 
   		for (int j = 1; j <=lag; ++j)
   		{
+  			// función para obtener siguiente valor
   			x=MHstep(x,sd);
   		}
+
+  		//muestra por pantalla valores obtenidos
   		cout << x << "\n";
   }
   
